04f6ffcc0e70dda6b158100f2572f26b5bf90c3f
Arbitel <2641332+chadleong@users.noreply.github.com>
2022-02-27 00:45:14 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import "./App.css"
+
+let name: string
+let age: number
+
+// name = "Chad"
+// name = 5
+
+function App() {
+    return <div className="App">Hello World</div>
+}
+
+export default App
